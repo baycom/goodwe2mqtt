2d1edcf4ec01e687f80a7f67cd3963b8c6859b1b
BayCom GmbH <software@baycom.de>
2022-11-01 00:04:44 +0100

    added RS485 communication & DT-/M-Series inverters

--- index.js
+++ index.js
@@ -5,21 +5,46 @@ var Parser = require('binary-parser').Parser;
 const commandLineArgs = require('command-line-args')
 
 const optionDefinitions = [
-	{ name: 'mqtthost', alias: 'm', type: String, defaultValue: "web" },
+	{ name: 'mqtthost', alias: 'm', type: String, defaultValue: "localhost" },
 	{ name: 'mqttclientid', alias: 'c', type: String, defaultValue: "goodwe1Client" },
-	{ name: 'inverterhost', alias: 'i', type: String, defaultValue:  "goodwe1"},
-	{ name: 'address', alias: 'a', type: Number , defaultValue: 1}
+	{ name: 'inverterhost', alias: 'i', type: String},
+	{ name: 'inverterport', alias: 'p', type: String},
+	{ name: 'type',         alias: 't', type: String, multiple: true, defaultValue: ['ET']}, 
+        { name: 'address',      alias: 'a', type: Number, multiple: true, defaultValue: [1] },
+        { name: 'wait',         alias: 'w', type: Number, defaultValue: 10000 },
+        { name: 'debug',        alias: 'd', type: Boolean, defaultValue: false },
   ];
 
 const options = commandLineArgs(optionDefinitions)
 
-var GWSerialNumber;
+var GWSerialNumber=[];
 var modbusClient = new ModbusRTU();
 
-console.log("MQTT host         : " + options.mqtthost);
+modbusClient.setTimeout(1000);
+
+if(options.inverterhost) {
+	modbusClient.connectTCP(options.inverterhost, { port: 502 }).catch((error) => {
+		console.error(error);
+		process.exit(-1);
+	});
+} else if(options.inverterport) {
+	modbusClient.connectRTUBuffered(options.inverterport, { baudRate: 9600, parity: 'none' }).catch((error) => {
+		console.error(error);
+		process.exit(-1);
+	});
+}
+
+console.log("MQTT Host         : " + options.mqtthost);
 console.log("MQTT Client ID    : " + options.mqttclientid);
-console.log("GoodWe host       : " + options.inverterhost);
+
 console.log("GoodWe MODBUS addr: " + options.address);
+console.log("GoodWe Type       : " + options.type);
+
+if(options.inverterhost) {
+	console.log("GoodWe host       : " + options.inverterhost);
+} else {
+	console.log("GoodWe serial port: " + options.inverterport);
+}
 
 var MQTTclient = mqtt.connect("mqtt://"+options.mqtthost,{clientId: options.mqttclientid});
 	MQTTclient.on("connect",function(){	
@@ -31,7 +56,14 @@ MQTTclient.on("error",function(error){
 		process.exit(1)
 	});
 
-const payloadParser = new Parser()
+function sendMqtt(id, data) {
+        if(options.debug) {
+	        console.log("publish: "+'GoodWe/' + id, JSON.stringify(data));
+	}
+        MQTTclient.publish('GoodWe/' + id, JSON.stringify(data));        
+}
+
+const ETPayloadParser = new Parser()
 	.seek((0x891F-0x891C)*2)
 	.uint16be('PV1Voltage', { formatter: (x) => {return x/10.0;}})
 	.uint16be('PV1Current', { formatter: (x) => {return x/10.0;}})
@@ -121,44 +153,139 @@ const payloadParser = new Parser()
 	.uint32be('DiagStatusH')
 	.uint32be('DiagStatusL')
 	;
-function getPayload(data) {
-	return payloadParser.parse(data);
+function getETPayload(data) {
+	return ETPayloadParser.parse(data);
 }
 
-function getSN() {
-	modbusClient.readHoldingRegisters(0x88BB, 8, function(err, vals) {
-		if(err) {
-			console.log(util.inspect(err));
-                        process.exit(1);
-		} else {		
-			GWSerialNumber = new String(vals.buffer);
+
+const getETSN = async (address) => {
+	try {
+		modbusClient.setID(address);
+		let vals = await modbusClient.readHoldingRegisters(0x88BB, 8);
+		GWSerialNumber[address] = new String(vals.buffer);
+		if(options.debug) {
 			console.log(GWSerialNumber);
-			setTimeout(MODBUSintervalFunc, 1000);
 		}
-	});
+	} catch (e) {
+		return null;
+	}
 }
 
-function sendMqtt(data) {
-	if(GWSerialNumber) {
-		MQTTclient.publish('GoodWe/' + GWSerialNumber, JSON.stringify(data));
-	}	
+const DTPayloadParser = new Parser()
+	.uint16be('PV1Voltage', { formatter: (x) => {return x/10.0;}})
+	.uint16be('PV2Voltage', { formatter: (x) => {return x/10.0;}})
+	.uint16be('PV1Current', { formatter: (x) => {return x/10.0;}})
+	.uint16be('PV2Current', { formatter: (x) => {return x/10.0;}})
+	.uint16be('GridL1Voltage', { formatter: (x) => {return x/10.0;}})
+	.uint16be('GridL2Voltage', { formatter: (x) => {return x/10.0;}})
+	.uint16be('GridL3Voltage', { formatter: (x) => {return x/10.0;}})
+	.uint16be('GridL1Current', { formatter: (x) => {return x/10.0;}})
+	.uint16be('GridL2Current', { formatter: (x) => {return x/10.0;}})
+	.uint16be('GridL3Current', { formatter: (x) => {return x/10.0;}})
+	.uint16be('GridL1Frequency', { formatter: (x) => {return x/100.0;}})
+	.uint16be('GridL2Frequency', { formatter: (x) => {return x/100.0;}})
+	.uint16be('GridL3Frequency', { formatter: (x) => {return x/100.0;}})
+	.uint16be('GridFeedingPowerL')
+	.uint16be('WorkMode')
+	.uint16be('Temperature', { formatter: (x) => {return x/10.0;}})
+        .uint32be('ErrorMessage')
+	.uint32be('ETotal', { formatter: (x) => {return x/10.0;}})
+	.uint32be('HTotal', { formatter: (x) => {return x/10.0;}})
+	.uint16be('Firmware')
+	.uint16be('Warning')
+	.uint16be('PV2FaultValue', { formatter: (x) => {return x/10.0;}})
+	.uint16be('FunctionsValue')
+	.uint16be('Line2VfaultValue', { formatter: (x) => {return x/10.0;}})
+	.uint16be('Line3VfaultValue', { formatter: (x) => {return x/10.0;}})
+	.uint16be('BUSVoltage', { formatter: (x) => {return x/10.0;}})
+	.uint16be('NBUSVoltage', { formatter: (x) => {return x/10.0;}})
+	.uint16be('Line3FfaultValue', { formatter: (x) => {return x/100.0;}})
+	.uint16be('SafetyCountry')
+	.uint16be('EDay', { formatter: (x) => {return x/10.0;}})
+	;
+function getDTPayload(data) {
+	return DTPayloadParser.parse(data);
 }
 
-function MODBUSintervalFunc () {
-	modbusClient.readHoldingRegisters(0x891C, 123, function(err, vals) {
-		if(err) {
-			console.log("readHoldingRegisters: " + util.inspect(err));
-			process.exit(1);
-		} else {
-			var gwState = getPayload(vals.buffer);
+const getDTSN = async (address) => {
+	try {
+		modbusClient.setID(address);
+		let vals = await modbusClient.readHoldingRegisters(0x200, 8);
+		GWSerialNumber[address] = new String(vals.buffer);
+		if(options.debug) {
+			console.log(GWSerialNumber);
+		}
+	} catch (e) {
+		return null;
+	}
+}
+
+const getETRegisters = async (address) => {
+	try {
+		modbusClient.setID(address);
+                let vals = await modbusClient.readHoldingRegisters(0x891C, 123);	
+		var gwState = getETPayload(vals.buffer);
+		if(options.debug) {
 			console.log(util.inspect(gwState));
-			sendMqtt(gwState);
-			setTimeout(MODBUSintervalFunc, 10000);
 		}
-	});
+		sendMqtt(GWSerialNumber[address], gwState);
+	} catch (e) {
+		return null;
+	}
 }
 
-modbusClient.connectTCP(options.inverterhost, { port: 502 });
-modbusClient.setID(options.address);
-modbusClient.setTimeout(1000);
-setTimeout(getSN, 1000);
+const getDTRegisters = async (address) => {
+	try {
+		modbusClient.setID(address);
+                let vals = await modbusClient.readHoldingRegisters(0x300, 0x21);
+		var gwState = getDTPayload(vals.buffer);
+		if(options.debug) {
+			console.log(util.inspect(gwState));
+		}
+		sendMqtt(GWSerialNumber[address], gwState);
+	} catch (e) {
+		return null;
+	}
+}
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const getMetersValue = async (meters) => {
+    try{
+        var pos=0;
+        // get value of all meters
+        for(let meter of meters) {
+                if(options.debug) {
+                        console.log("query: " + meter + " type: " + options.type[pos]);
+                }
+                if(!GWSerialNumber[meter]) {
+			if(options.type[pos] == 'DT') {
+				await getDTSN(meter);
+			} else {
+	                	await getETSN(meter);
+			}
+                }
+                if(GWSerialNumber[meter]) {
+                	if(options.type[pos] == 'DT') {
+                		await getDTRegisters(meter);
+                	} else {
+				await getETRegisters(meter);
+			}
+		}
+		pos++;
+        }
+	await sleep(options.wait);
+    } catch(e){
+        // if error, handle them here (it should not)
+        console.log(e)
+    } finally {
+        // after get all data from salve repeate it again
+        setImmediate(() => {
+            getMetersValue(meters);
+        })
+    }
+}
+
+// start get value
+getMetersValue(options.address);
+
